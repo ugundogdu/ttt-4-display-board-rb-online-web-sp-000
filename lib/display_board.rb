da26bc89c5ca571ab990c478a9814b531bde6c6b
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-01 15:39:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  puts " #{board[0]} #{board[1]} #{board[2]} "
-  puts " #{board[3]} #{board[4]} #{board[5]}"
-  puts " #{board[6]} #{board[7]} #{board[8]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
